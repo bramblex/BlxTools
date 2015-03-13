378335e7e1459f5e848538c9517a4e96d8902bc3
brambles <qjnight@gmail.com>
2015-03-13 23:37:55 +0800

    aaa

--- javascripts/setup.js
+++ javascripts/setup.js
@@ -1,4 +1,4 @@
 (function(){
   $('pre').addClass('prettyprint');
-  $('<script></script>').attr('src', 'https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/run_prettify.min.js');
+  $('head').append($('<script>').attr('src', 'https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/run_prettify.min.js'));
 })();
