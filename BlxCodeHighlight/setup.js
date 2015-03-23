1a2b695082d30915777253e483c6fff907618a18
brambles <qjnight@gmail.com>
2015-03-23 17:12:07 +0800

    代码高亮脚本

--- BlxCodeHighlight/setup.js
+++ BlxCodeHighlight/setup.js
@@ -1,4 +1,14 @@
 (function(){
-  $('pre').addClass('prettyprint');
-  $('<script></script>').attr('src', 'https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/run_prettify.min.js');
+  $('pre').addClass('prettyprint linenums');
+
+  $('head').append($('<link rel="stylesheet" type="text/css" media="screen" href="http://bramblex.github.io/BlxTools/cdn/prettify/prettify.css">'));
+  $('head').append($('<script src="http://bramblex.github.io/BlxTools/cdn/prettify/prettify.js"></script>'));
+
+  var timer = setInterval(function(){
+    if ( typeof(prettyPrint) === 'function' ){
+      prettyPrint();
+      clearInterval(timer);
+    }
+  }, 100);
+
 })();
